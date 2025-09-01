de3620e84a16083b54891e84e56875344301a98c
Kilian Gosewisch <kilian2798@gmail.com>
2025-09-01 12:56:05 +0200

    added example scripts

---
+++ examples/copy_to_devices.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##########
+# this script will sync your covered/converted songs to the first found known device.
+# this example uses udisksctl,
+# usually used by the major file managers to auto show/mount your removable disks.
+# this script uses the same mechanism to mount your device without needing root permissions.
+# then it uses rsync to synch your device with the output of rocksonic
+##########
+
+# put your devices block devices here, the script will be looking for them to mount them automatically
+KNOWN_DEVICES=(
+  /dev/disk/by-id/usb-SST55LD0_19K-45-C-MWE_100000000000A270018445AAA-0:0-part2
+)
+FOUND=""
+for device in "${KNOWN_DEVICES[@]}"; do
+  if ls "$device" &>/dev/null; then
+    FOUND="$device"
+    break
+  fi
+done
+
+if [[ "$FOUND" == "" ]]; then
+  echo "no known device found. exiting..."
+  exit
+fi
+UDISKOUTPUT=$(udisksctl mount -b "${FOUND}" -o rw 2>&1)
+if [ "$?" = "1" ]; then
+  if echo "$UDISKOUTPUT" | grep already &>/dev/null; then
+    echo "device already mounted. continuing"
+  else
+    exit
+  fi
+fi
+
+MOUNT=$(echo "$UDISKOUTPUT" | awk -F'at ' '{print $2}' | sed 's/\.$//' | sed "s/'$//" | sed 's/`//')
+rsync -rh ./rocksonic_songs/converted/combined/ "$MOUNT/rocksonic/" --delete --update --size-only --info=progress2
+sync
+DEVICE=$(findmnt -n -o SOURCE "$MOUNT")
+udisksctl unmount -b "$DEVICE"
