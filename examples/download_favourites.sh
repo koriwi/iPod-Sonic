2ca4c2e0cb8eb4e14576d53d1042ca954549a6a2
Kilian Gosewisch <kilian2798@gmail.com>
2025-09-12 00:07:03 +0200

    rename main to rocksonic

--- examples/download_favourites.sh
+++ examples/download_favourites.sh
@@ -2,4 +2,4 @@
 
 # download your liked songs, convert to mp3 with quality 3 [1 is highest, 10 is lowest], and embedd covers with a width of 200px
 
-./main -url https://music.navidrome.example/rest -user jon -pass doe -mp3 -quality 3 -coversize 400
+./rocksonic -url https://music.navidrome.example/rest -user jon -pass doe -mp3 -quality 3 -coversize 400
